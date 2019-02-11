c8e33f6b88cc753b23376aaa3e225863dc76f9fe
Isochore <jules.p@codeur.online>
2019-02-11 15:37:53 +0000

    intégration mon compte

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -29,10 +29,41 @@ class FrontController extends AbstractController
 
     /**
      * @Route("/mon-compte", name="mon_compte")
+     * @Route("/mon-compte/informations", name="compte-informations")
      */
     public function showAccount()
     {
         return $this->render('front/mon-compte.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+
+    /**
+     * @Route("/mon-compte/adresses", name="compte-adresses")
+     */
+    public function showAdresses()
+    {
+        return $this->render('front/compte-adresses.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+
+    /**
+     * @Route("/mon-compte/ajouter-adresses", name="ajouter-adresses")
+     */
+    public function addAdresses()
+    {
+        return $this->render('front/ajouter-adresses.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+
+    /**
+     * @Route("/mon-compte/commandes", name="compte-commandes")
+     */
+    public function showCommandes()
+    {
+        return $this->render('front/compte-commandes.html.twig', [
             'controller_name' => 'FrontController',
         ]);
     }
