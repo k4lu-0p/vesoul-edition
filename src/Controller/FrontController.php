c3951f95a886ab3dea3d9e8aad9cf401136aef2d
Isochore <jules.p@codeur.online>
2019-02-11 08:50:44 +0000

    merge

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -16,4 +16,24 @@ class FrontController extends AbstractController
             'controller_name' => 'FrontController',
         ]);
     }
+
+    /**
+     * @Route("/product", name="product")
+     */
+    public function showProduct()
+    {
+        return $this->render('front/product.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+
+    /**
+     * @Route("/mon-compte", name="mon_compte")
+     */
+    public function showAccount()
+    {
+        return $this->render('front/mon-compte.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
 }
