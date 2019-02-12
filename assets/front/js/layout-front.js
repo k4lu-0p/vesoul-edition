fa22d9f8da78e1d48d379ac6f6be0b10b59bb143
Louya <fabien.c@codeur.online>
2019-02-12 13:10:39 +0000

    [fix](index.html.twig, index.scss), Correction de certains détails de style

--- assets/front/js/layout-front.js
+++ assets/front/js/layout-front.js
@@ -4,4 +4,3 @@ require('../../../node_modules/bootstrap/dist/js/bootstrap.js');
 require('../../../node_modules/bootstrap/dist/css/bootstrap.min.css');
 require('../css/main-front.scss');
 require('./main-front.ts');
-//require('./index.ts');
