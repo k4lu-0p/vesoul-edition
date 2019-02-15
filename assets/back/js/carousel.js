2253024cbd0442c84df2fce90f1752109ccaca99
Mflantier <antonin.l@codeur.online>
2019-02-15 15:46:58 +0000

    [FEAT] carousel lightbox details livre, non fonctionnel

---
+++ assets/back/js/carousel.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    navigation:false,
+    autoWidth: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
