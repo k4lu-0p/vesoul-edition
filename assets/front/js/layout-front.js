6a7365576543b17685e7005b6ed313fd5ff8f84f
Isochore <jules.p@codeur.online>
2019-02-18 16:00:24 +0000

    homogénéisation et modal

--- assets/front/js/layout-front.js
+++ assets/front/js/layout-front.js
@@ -2,6 +2,8 @@ require('../../../node_modules/jquery/dist/jquery.slim.js');
 require('../../../node_modules/popper.js/dist/popper.min.js');
 require('../../../node_modules/bootstrap/dist/js/bootstrap.js');
 require('../../../node_modules/bootstrap/dist/css/bootstrap.min.css');
+// require('../../../node_modules/materialize-css/dist/css/materialize.css');
+// require('../../../node_modules/materialize-css/dist/js/materialize.js');
 require('../css/main-front.scss');
 require('../../../node_modules/air-datepicker/dist/css/datepicker.min.css');
 require('../../../node_modules/air-datepicker/dist/js/datepicker');
