a9a666d233b4a72f67f75e91566dbcd105e785fa
Mflantier <antonin.l@codeur.online>
2019-02-18 08:47:03 +0000

    [FEAT] install slick-carousel + uninstall owl.carousel

--- assets/back/js/carousel.js
+++ assets/back/js/carousel.js
@@ -1,17 +1,15 @@
-$('.owl-carousel').owlCarousel({
-    loop:false,
-    margin:10,
-    navigation:false,
-    autoWidth: true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
-})
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
--- assets/back/js/layout-back.js
+++ assets/back/js/layout-back.js
@@ -2,8 +2,9 @@ require('../../../node_modules/jquery/dist/jquery.slim.js');
 require('../../../node_modules/popper.js/dist/popper.min.js');
 require('../../../node_modules/bootstrap/dist/js/bootstrap.js');
 require('../../../node_modules/bootstrap/dist/css/bootstrap.min.css');
-require('../../../node_modules/owl.carousel/dist/owl.carousel.min.js')
-require('../../../node_modules/owl.carousel/dist/assets/owl.carousel.min.css')
+require('../../../node_modules/slick-carousel/slick/slick.css')
+require('../../../node_modules/slick-carousel/slick/slick-theme.css')
+require('../../../node_modules/slick-carousel/slick/slick.min.js')
 require('../css/main-back.scss');
 require('./main-back.ts');
 require('./carousel.js')
