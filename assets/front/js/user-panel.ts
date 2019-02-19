3d42be287c5a6bdf0637621fb7a196d304e4938c
lucas-acs <lucas.rob1@live.fr>
2019-02-19 08:26:14 +0000

    [FIX] Fix Typescript input type field

--- assets/front/js/user-panel.ts
+++ assets/front/js/user-panel.ts
@@ -1,5 +1,5 @@
-let checkHomme = document.querySelector(".homme");
-let checkFemme = document.querySelector(".femme");
+let checkHomme = document.querySelector(".homme") as HTMLInputElement;
+let checkFemme = document.querySelector(".femme") as HTMLInputElement;
 
 checkHomme.addEventListener('click', () => {
     checkFemme.checked = false;
