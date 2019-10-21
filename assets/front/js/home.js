b7dd6d8c8bcb029f046d4397c371dda3d02a0479
keolite <e.baillysalins@outlook.fr>
2019-10-21 12:45:58 +0000

    Gestion des filtres sur la page search

--- assets/front/js/home.js
+++ assets/front/js/home.js
@@ -233,10 +233,20 @@ btnApplyFilter.addEventListener('click', function(){
   applyYearFilter();
   orderBy = itemList.value;
   page = 1;
+
+  //Si je suis sur la page search
+  //Ajouter le titre dans le filter
+  if( document.querySelector('.search-keyword')){
+    elSearchKeyword = document.querySelector('.search-keyword');
+    searchKeyword = elSearchKeyword.innerText.slice(1,-1);
+    filter.title = searchKeyword;
+  }
+
   wrapperBooks.innerHTML = '';
   loader.classList.add("loader-on");
   fetchBooks();
   ticking = true;
+
 });
 
 
@@ -542,7 +552,12 @@ function applyYearFilter(){
           elTotalBooks = document.querySelector('#totalPage')
           elTotalBooks.innerHTML = totalBooks;
           if( totalBooks == 0 ){
-            wrapperBooks.innerHTML += `<p class="p-4 ml-2 mt-2 alert alert-dark" > Aucun livre ne correspond à vos critères de recherches. <span class="font-italic" >Modifiez vos critères et cliquez</span> à nouveau sur <span class="font-weight-bold" >'Appliquer les filtres'</span> ou cliquez sur le bouton <span class="font-weight-bold" >'Enlever tous les filtres'.</span></p>`;
+            if( document.querySelector('.search-keyword') !== null ){
+              wrapperBooks.innerHTML += `<p class="p-4 ml-2 mt-2 alert alert-dark" > Aucun livre ne correspond à votre recherche.  <span class="font-italic" >Modifier vos mots-clés</span> et <span class="font-weight-bold" >rechercher à nouveau</span> ou cliquez sur le lien <span class="font-weight-bold" >'Afficher tous les livres'.</span></p>
+              <p><a href="/"  class="btn btn-info btn-lg btn-block">Afficher tous les livres</a></p>`;
+            }else{
+              wrapperBooks.innerHTML += `<p class="p-4 ml-2 mt-2 alert alert-dark" > Aucun livre ne correspond à vos critères de recherches. <span class="font-italic" >Modifiez vos critères et cliquez</span> à nouveau sur <span class="font-weight-bold" >'Appliquer les filtres'</span> ou cliquez sur le bouton <span class="font-weight-bold" >'Enlever tous les filtres'.</span></p>`;
+            }
             loader.classList.remove('loader-on');
             page = 1;
             ticking = false;
